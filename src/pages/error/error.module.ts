e3a36fc78850119ba8f333486419120272a54dea
Unknown <jefferson30.jdg@gmail.com>
2019-01-19 01:52:46 +0800

    Added Header Component

--- src/pages/error/error.module.ts
+++ src/pages/error/error.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ErrorPage } from './error';
+import { HeaderComponent } from './../../components/header/header';
 
 @NgModule({
   declarations: [
     ErrorPage,
+    HeaderComponent,
   ],
   imports: [
     IonicPageModule.forChild(ErrorPage),
