c96f289cb92e5fe71844bbbb162ff7699366ceec
Unknown <jefferson30.jdg@gmail.com>
2019-01-19 01:53:17 +0800

    Added header component

--- src/pages/send/send.module.ts
+++ src/pages/send/send.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { SendPage } from './send';
+import { HeaderComponent } from './../../components/header/header';
 
 @NgModule({
   declarations: [
     SendPage,
+    HeaderComponent,
   ],
   imports: [
     IonicPageModule.forChild(SendPage),
