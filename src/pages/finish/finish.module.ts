1b76c96699ba3489188f643add14e9821d70cd53
Unknown <jefferson30.jdg@gmail.com>
2019-01-19 01:52:57 +0800

    Added Header Component

--- src/pages/finish/finish.module.ts
+++ src/pages/finish/finish.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { FinishPage } from './finish';
+import { HeaderComponent } from './../../components/header/header';
 
 @NgModule({
   declarations: [
     FinishPage,
+    HeaderComponent,
   ],
   imports: [
     IonicPageModule.forChild(FinishPage),
