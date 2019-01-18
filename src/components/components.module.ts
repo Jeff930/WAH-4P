561088c506d97f54f53007771dc34fef7a3a2e10
Unknown <jefferson30.jdg@gmail.com>
2019-01-19 01:51:50 +0800

    Added Header Component to Component Module

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from './header/header';
 import { LgUnameComponent } from './lg-uname/lg-uname';
+import { IonicModule } from 'ionic-angular';
 @NgModule({
 	declarations: [HeaderComponent,
     HeaderComponent,
     LgUnameComponent],
-	imports: [],
+	imports: [IonicModule],
 	exports: [HeaderComponent,
     HeaderComponent,
     LgUnameComponent]
