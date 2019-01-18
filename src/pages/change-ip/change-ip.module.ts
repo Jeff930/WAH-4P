c69938358193d02248a31435a1bc847c698b6f9b
Unknown <jefferson30.jdg@gmail.com>
2019-01-19 01:52:34 +0800

    Added Header component

--- src/pages/change-ip/change-ip.module.ts
+++ src/pages/change-ip/change-ip.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ChangeIpPage } from './change-ip';
+import { HeaderComponent } from './../../components/header/header';
+
 
 @NgModule({
   declarations: [
     ChangeIpPage,
+    HeaderComponent,
   ],
   imports: [
     IonicPageModule.forChild(ChangeIpPage),
