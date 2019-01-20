012347349dd260fce52e0bdba68781ab87b30169
Unknown <jefferson30.jdg@gmail.com>
2019-01-20 23:29:13 +0800

    Redesign of home page

--- src/pages/home/home.module.ts
+++ src/pages/home/home.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { HomePage } from './home';
 import { HeaderComponent } from './../../components/header/header';
+import { LguNameComponent } from './../../components/lgu-name/lgu-name';
 
 @NgModule({
   declarations: [
     HomePage,
     HeaderComponent,
+    LguNameComponent,
   ],
   imports: [
     IonicPageModule.forChild(HomePage),
