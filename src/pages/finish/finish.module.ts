0fefb7bb5cd0d4f287c5b566eee1896db91a1c2f
Unknown <jefferson30.jdg@gmail.com>
2019-01-22 01:54:13 +0800

    Added LGU name component

--- src/pages/finish/finish.module.ts
+++ src/pages/finish/finish.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { FinishPage } from './finish';
 import { HeaderComponent } from './../../components/header/header';
+import { LguNameComponent } from './../../components/lgu-name/lgu-name';
 
 @NgModule({
   declarations: [
     FinishPage,
     HeaderComponent,
+    LguNameComponent,
   ],
   imports: [
     IonicPageModule.forChild(FinishPage),
