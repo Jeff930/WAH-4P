2d715097dbadcbcd487842e23e47a2daba60c04a
Unknown <jefferson30.jdg@gmail.com>
2019-01-23 01:41:38 +0800

    Added Ionic 3 native SMS plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { SMS } from '@ionic-native/sms';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -39,6 +40,7 @@ import { ComponentsModule } from './../components/components.module';
   providers: [
     StatusBar,
     SplashScreen,
+    SMS,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     SenderProvider
   ]
