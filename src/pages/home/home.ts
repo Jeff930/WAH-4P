be6ca3bd4cf12e871abcb95a804c33474caad863
Unknown <jefferson30.jdg@gmail.com>
2019-02-06 01:23:41 +0800

    Added method for navigation to Send page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,7 +6,6 @@ import { SendPage } from '../send/send';
 import { SMS } from '@ionic-native/sms';
 import { SenderProvider} from '../../providers/sender/sender';
 import { HttpClient } from '@angular/common/http';
-
 /**
  * Generated class for the HomePage page.
  *
@@ -21,19 +20,19 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomePage {
 
-  homeTimer;
-  homeTimeout;
+  public homeTimer;
+  public homeTimeout;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public sms: SMS, public sender:SenderProvider,public http: HttpClient) {
   }
 
   ionViewDidEnter() {
     console.log('ionViewDidEnter HomePage');
+    this.getLGU();
     this.getData();
   }
 
   getData(){
-    this.getLGU();
     this.homeTimeout=setTimeout(() => { 
       console.log("homecalled");
       this.getMessages();}, 5000);
@@ -47,20 +46,25 @@ export class HomePage {
   getLGU(){
     this.http.get('../../assets/RHU.json').toPromise()
     //this.http.get('http://'+'/api/spasms/getRHU').toPromise()
-    .then((data:any)=> {
-      this.sender.LGU_Name = data[0].hfhudname; 
-      console.log(this.sender.LGU_Name);
-  },error=>{this.sender.LGU_Name = "**Cannot retrieve RHU name**";
+    .then((rhuName:any)=> {
+      console.log(rhuName[0].hfhudname);
+      this.sender.setRhuName(rhuName[0].hfhudname); 
+  },error=>{
+    this.sender.setRhuName("**Cannot retrieve RHU name**");
     console.log("Cannot retrieve RHU name");
 });}
 
 getMessages(){
-  this.http.get('../../assets/sampe.json').toPromise()
+  this.http.get('../../assets/sample.json').toPromise()
   //this.http.get('http://'+'/api/spasms/getRHU').toPromise()
-  .then((data:any)=> {
-    console.log(JSON.stringify(data));
-    this.sender.sendMessages(data); 
-    
+  .then((messageDetails:any)=> {
+    if (messageDetails.length==0){
+      this.getData();
+    }else{
+      console.log(JSON.stringify(messageDetails));
+      this.sender.setMessages(messageDetails); 
+      this.navCtrl.setRoot(SendPage);
+    }  
 },error=>{console.log("Cannot retrieve messages");
   this.navCtrl.setRoot(ErrorPage);
 });
