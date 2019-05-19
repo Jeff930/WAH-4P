15700148158d6e3d51ea5d4a77f8b156df6991df
Unknown <jefferson30.jdg@gmail.com>
2019-05-19 22:32:43 +0800

    Added sms package

--- src/pages/send/send.ts
+++ src/pages/send/send.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { SenderProvider} from '../../providers/sender/sender';
+import { SMS } from '@ionic-native/sms';
 
 /**
  * Generated class for the SendPage page.
@@ -18,18 +19,28 @@ export class SendPage {
 
   public progress;
   public messageDetails;
-  constructor(public navCtrl: NavController, public navParams: NavParams,public sender:SenderProvider,) {
+  public totalMessages;
+  public sentMessages = 0;
+  constructor(public navCtrl: NavController, public navParams: NavParams,public sender:SenderProvider,public sms: SMS) {
   }
 
-  ionViewDidEnter() {
+  ionViewWillEnter() {
     console.log('ionViewDidEnter SendPage');
     this.messageDetails=this.sender.getMessages();
     console.log(this.messageDetails);
+    console.log(this.messageDetails.length);
+    this.totalMessages = this.messageDetails.length;
     this.sendMessage();
   }
 
   sendMessage(){
-    this.messageDetails.length;
+    for (var i = 0;i<this.totalMessages;i++){
+      console.log(this.messageDetails[i]);
+      console.log(this.messageDetails[i].msg);
+      console.log(this.messageDetails[i].receiver);
+      //this.sms.send(this.messageDetails[i].receiver,this.messageDetails[i].msg);
+    }
+    
   }
 
 }
